90873d47c79cb0dd0857029c2a0b7f8c09ebed70
Jusarine <tgladka6@gmail.com>
2018-08-11 21:56:28 +0300

    Initial commit

---
+++ src/main/java/com/artschool/ArtSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.artschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtSchoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtSchoolApplication.class, args);
+    }
+
+}
