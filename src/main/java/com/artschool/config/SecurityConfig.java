d97bf174cdc72b4de42c2237d452f4b076ddebb8
Jusarine <tgladka6@gmail.com>
2018-09-20 19:48:30 +0300

    enable CSRF protection

--- src/main/java/com/artschool/config/SecurityConfig.java
+++ src/main/java/com/artschool/config/SecurityConfig.java
@@ -45,8 +45,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf()
-                    .disable()
                 .authorizeRequests()
                 .antMatchers("/")
                     .permitAll()
--- src/main/resources/static/js/x_editable.js
+++ src/main/resources/static/js/x_editable.js
@@ -1,7 +1,14 @@
 $(document).ready(function() {
+    var token = $("meta[name='_csrf']").attr("content");
+    var header = $("meta[name='_csrf_header']").attr("content");
     $('.x_editable').editable({
         mode: 'inline',
-        emptytext: 'What are you currently working on?'
+        emptytext: 'What are you currently working on?',
+        ajaxOptions: {
+            beforeSend: function(xhr) {
+                xhr.setRequestHeader(header, token);
+            }
+        }
     });
     $.fn.editableform.buttons =
         '<button type="submit" class="btn btn-primary btn-sm editable-submit">'+
